28ed5948509f79cc377773771e9c0c0df08e3618
Tanmay <tanmay123@gmail.com>
2026-02-14 15:16:08 +0530

    setup basic config

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home(){
+  return <>
+  <h1>Hello World</h1>
+  <Button variant="destructive">CLick O</Button>
+  <p className="bg-black text-white inline">Telenee</p>
+  </>
+}
